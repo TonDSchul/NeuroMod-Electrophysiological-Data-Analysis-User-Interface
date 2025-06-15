19d926f1a22005b0e66a0d7aa74d45c5726ac4a0
Tony de Schultz <tony_deschultz@web.de>
2025-06-15 23:51:20 +0200

    Importing Events

--- Modules/5. Event_Related_Module/Import_Events_Window/Functions/Import_Events_Add_Imported_Events.m
+++ Modules/5. Event_Related_Module/Import_Events_Window/Functions/Import_Events_Add_Imported_Events.m
@@ -7,6 +7,7 @@ function [Data,EventChannelDropDown] = Import_Events_Add_Imported_Events(Data,Ev
 % Department systemsphysiology of learning, LIN Magdeburg.
 %________________________________________________________________________________________
 
+
 %% First maintaining GUI main data structure by deleting previous event data
 if isfield(Data,'Events')
     msgbox("Warning: Events where already extracted. Previous data will be overwritten!");
@@ -48,12 +49,47 @@ if isfield(Data,'PreprocessedEventRelatedData')
     Data.Info = rmfield(Data.Info, fieldsToDelete);
 end
 
-Data.Events = EventInfo.TempEvents;
-Data.Info.EventChannelType = "Imported File";
-
+%% check for events outside of range
 eventnames = strsplit(EventInfo.EventNames,',');
 
+for i = 1:length(EventInfo.TempEvents)
+
+    deletedevents = EventInfo.TempEvents{i}(EventInfo.TempEvents{i} > Data.Info.num_data_points);
+
+    EventInfo.TempEvents{i}(EventInfo.TempEvents{i} > Data.Info.num_data_points) = [];
+
+    if ~isempty(deletedevents)
+        msgbox(strcat("Event ",eventnames{i}," contains ",num2str(length(deletedevents))," event indices outside the time range that are deleted."))
+    end
+end
+
+Celltodelete = [];
+for i = 1:length(EventInfo.TempEvents)
+    if isempty(EventInfo.TempEvents{i})
+        Celltodelete = [Celltodelete,i];
+    end
+end
+
+if ~isempty(Celltodelete)
+    for i = 1:length(Celltodelete)
+        if length(EventInfo.TempEvents) > 1
+            EventInfo.TempEvents(Celltodelete) = [];
+            eventnames(Celltodelete) = [];
+        else
+            EventChannelDropDown{1} = "";
+            msgbox("Non of the event channel contain suitable indices. No events extracted.")
+            if isfield(Data,'Events')
+                fieldToRemove = 'Events';
+                Data = rmfield(Data, fieldToRemove);
+            end
+            return
+        end
+    end
+end
+
 Data.Info.EventChannelNames = eventnames;
+Data.Events = EventInfo.TempEvents;
+Data.Info.EventChannelType = "Imported File";
 
 for i = 1:length(eventnames)
     EventChannelDropDown{i} = eventnames{i};
